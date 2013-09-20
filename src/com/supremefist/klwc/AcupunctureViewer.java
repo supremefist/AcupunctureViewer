0d245e881665cb0f80a3184dc8dfbef7d5fee861
supremefist <sakkie99@gmail.com>
2013-09-20 23:36:34 +0200

    Initial basic swing window.

--- src/com/supremefist/klwc/AcupunctureViewer.java
+++ src/com/supremefist/klwc/AcupunctureViewer.java
@@ -1,9 +1,34 @@
 package com.supremefist.klwc;
 
-/*
- * Controller class for Acupuncture Viewer.
- */
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class AcupunctureViewer {
+    /*
+     * Controller class for Acupuncture Viewer.
+     */
+    
+    private View view;
+    
+    public AcupunctureViewer() {
+        view = new View();
+    }
+    
+    public void start() {
+        view.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                AcupunctureViewer app = new AcupunctureViewer();
+                app.start();
+            }
+        });
+    }
+}    
 
-}
+
+
---
+++ src/com/supremefist/klwc/View.java
@@ -0,0 +1,13 @@
+package com.supremefist.klwc;
+
+import javax.swing.JFrame;
+
+public class View extends JFrame {
+    public View() {
+        
+        setTitle("KLWC Acupuncture Viewer");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);        
+    }
+}
